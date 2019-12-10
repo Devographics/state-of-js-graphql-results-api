4e005178ae6282c20a6a8d6208e2fd1811c2a1e4
SachaG <358832+SachaG@users.noreply.github.com>
2019-12-10 21:25:16 +0900

    Fix feature db path

--- src/resolvers/feature.mjs
+++ src/resolvers/feature.mjs
@@ -5,9 +5,9 @@ export default {
         allYears: async (feature, args, context, info) => {
             return await computeFeatureUsageByYear(context.db, feature.id)
         },
-        year: async (feature, args, context, info) => {
+        year: async (feature, { year }, context, info) => {
             const allYears = await computeFeatureUsageByYear(context.db, feature.id)
-            return allYears.find(yearItem => yearItem.year === args.year)
+            return allYears.find(y => y.year === year)
         }
     }
 }
