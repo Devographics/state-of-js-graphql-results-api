18b98624f92d05c01995a68d077e513bd2a4fd14
Raphaël Benitte <501642+plouc@users.noreply.github.com>
2019-12-28 09:05:57 +0900

    Migrate features experience to TypeScript

--- src/resolvers/features.ts
+++ src/resolvers/features.ts
@@ -1,26 +1,31 @@
-// import { computeFeatureUsageByYear } from '../analysis'
-// import { useCache } from '../caching'
+import { useCache } from '../caching'
 import { fetchMdnResource } from '../external_apis'
 import features from '../data/features.yml'
+import { RequestContext, SurveyConfig } from '../types'
+import { computeTermAggregationByYear } from '../compute'
 
 export default {
-    /*
     FeatureExperience: {
-        allYears: async (feature, args, context, info) => {
-            return getCachedResult(computeFeatureUsageByYear, context.db, [
-                feature.id,
-                feature.survey
-            ])
+        allYears: async (
+            { id, survey }: { id: string; survey: SurveyConfig },
+            args: any,
+            { db }: RequestContext
+        ) => {
+            return useCache(computeTermAggregationByYear, db, [survey, `features.${id}`])
         },
-        year: async (feature, { year }, context, info) => {
-            const allYears = await getCachedResult(computeFeatureUsageByYear, context.db, [
-                feature.id,
-                feature.survey
+        year: async (
+            { id, survey }: { id: string; survey: SurveyConfig },
+            { year }: { year: number },
+            { db }: RequestContext
+        ) => {
+            const allYears = await useCache(computeTermAggregationByYear, db, [
+                survey,
+                `features.${id}`
             ])
+
             return allYears.find(y => y.year === year)
         }
     },
-    */
     Feature: {
         name: ({ id }: { id: string }) => {
             const feature = features.find(f => f.id === id)
