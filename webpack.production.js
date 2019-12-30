3074551e94c78a3298aa55432607921b3c557048
SachaG <358832+SachaG@users.noreply.github.com>
2019-12-31 07:19:51 +0900

    fix heroku build

--- webpack.production.js
+++ webpack.production.js
@@ -1,4 +1,4 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const merge = require('webpack-merge')
 
 const common = require('./webpack.common.js')
