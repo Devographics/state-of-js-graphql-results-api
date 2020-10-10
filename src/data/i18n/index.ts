fa404b2db5850f3caaed2b0a4ab71f12a3085ecf
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-10 15:21:22 +0900

    Use enum for locales

--- src/data/i18n/index.ts
+++ src/data/i18n/index.ts
@@ -5,13 +5,13 @@ import { default as enCSS } from './state-of-css/yml/en-US.yml'
 import { default as enJS } from './state-of-js/yml/en-US.yml'
 import { default as enHomepage } from './homepage/yml/en-US.yml'
 
-// import { default as itCommon } from './common/yml/it-IT.yml';
-// import { default as itCSS } from './state-of-css/yml/it-IT.yml';
-// import { default as itJS } from './state-of-js/yml/it-IT.yml';
+import { default as itCommon } from './common/yml/it-IT.yml';
+import { default as itCSS } from './state-of-css/yml/it-IT.yml';
+import { default as itJS } from './state-of-js/yml/it-IT.yml';
 
-// import { default as zhCommon } from './common/yml/zh-Hans.yml';
-// import { default as zhCSS } from './state-of-css/yml/zh-Hans.yml';
-// import { default as zhJS } from './state-of-js/yml/zh-Hans.yml';
+import { default as zhCommon } from './common/yml/zh-Hans.yml';
+import { default as zhCSS } from './state-of-css/yml/zh-Hans.yml';
+import { default as zhJS } from './state-of-js/yml/zh-Hans.yml';
 
 const locales: Locale[] = [
     {
@@ -20,18 +20,18 @@ const locales: Locale[] = [
         label: 'English',
         stringFiles: [enCommon, enCSS, enJS, enHomepage],
     },
-    // {
-    //   id: 'it',
-    //   locale: 'it_IT',
-    //   label: 'Italiano',
-    //   stringFiles: [itCommon, itCSS, itJS],
-    // },
-    // {
-    //   id: 'zh',
-    //   locale: 'zh_ZH',
-    //   label: '中文',
-    //   stringFiles: [zhCommon, zhCSS, zhJS],
-    // },
+    {
+      id: 'it',
+      locale: 'it_IT',
+      label: 'Italiano',
+      stringFiles: [itCommon, itCSS, itJS],
+    },
+    {
+      id: 'zh',
+      locale: 'zh_ZH',
+      label: '中文',
+      stringFiles: [zhCommon, zhCSS, zhJS],
+    },
 ]
-const f = 123
+
 export default locales
