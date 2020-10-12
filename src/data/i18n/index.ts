cff07b883baf6a8073be58d218a26861abcbd80b
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-13 07:17:47 +0900

    Add projects/features/entities to locale query

--- src/data/i18n/index.ts
+++ src/data/i18n/index.ts
@@ -1,14 +1,20 @@
 import localesYAML from './locales.yml'
+import projects from '../projects.yml'
+import features from '../features.yml'
+import entities from '../entities.yml'
 
+const allEntities: any = { projects, features, entities }
 const locales: any = localesYAML
 
-Object.keys(locales).forEach(l => {
-  locales[l].stringFiles = []
-})
+const convertToStrings = (file: any) => file.map((item: any) => ({ key: item.id, t: item.name }))
 
-function importAll(r: any) {
-    r.keys().forEach((key: any) => (locales[key] = r(key)))
-}
+Object.keys(locales).forEach(l => {
+    // initialize every locale with projects, entities, and features
+    locales[l].stringFiles = Object.keys(allEntities).map(e => ({
+        context: e,
+        strings: convertToStrings(allEntities[e])
+    }))
+})
 
 const contexts: any = {
     common: require.context('./common/', true, /\.yml$/),
@@ -21,16 +27,16 @@ const contexts: any = {
 Object.keys(contexts).forEach(context => {
     const req = contexts[context]
     req.keys().forEach((key: any) => {
-        const localeName = key.replace('./', '').replace('.yml', '');
+        const localeName = key.replace('./', '').replace('.yml', '')
         if (localeName === 'model') {
-          // do nothing
+            // do nothing
         } else if (locales[localeName]) {
-          locales[localeName].stringFiles.push({
-            strings: req(key),
-            context
-          });
+            locales[localeName].stringFiles.push({
+                strings: req(key),
+                context
+            })
         } else {
-          throw new Error(`Locale ${localeName} not declared in locales.yml`)
+            throw new Error(`Locale ${localeName} not declared in locales.yml`)
         }
     })
 })
