1851cdd9fc23aa249226c41170b34936cdb565ef
SachaG <358832+SachaG@users.noreply.github.com>
2020-11-05 20:41:45 +0900

    More entities clean up

--- src/data/entities/index.ts
+++ src/data/entities/index.ts
@@ -11,26 +11,39 @@ import text_editors from './text_editors.yml'
 import youtube from './youtube.yml'
 import missing_features from './missing_features.yml'
 import sources from './sources.yml'
-
+import people from './people.yml'
+import courses from './courses.yml'
+import newsletters from './newsletters.yml'
+import socialmedia from './socialmedia.yml'
 
 const entities: any = {
-  browsers,
-  features,
-  languages,
-  libraries,
-  other,
-  podcasts,
-  sites,
-  text_editors,
-  youtube,
-  missing_features,
-  sources,
+    browsers,
+    features,
+    languages,
+    libraries,
+    other,
+    podcasts,
+    sites,
+    text_editors,
+    youtube,
+    missing_features,
+    sources,
+    people,
+    courses,
+    newsletters,
+    socialmedia,
 }
 
 // add key as category and flatten the whole thing
-const allEntities: Entity[] = Object.keys(entities).map((category: string) => {
-  const catEntities = entities[category]
-  return catEntities.map((e: Entity) => ({...e, category}))
-}).flat()
+const allEntities: Entity[] = Object.keys(entities)
+    .map((category: string) => {
+        const catEntities = entities[category]
+        return catEntities.map((e: Entity) => {
+            // add category as a tag
+            const tags = e.tags ? [...e.tags, category] : [category]
+            return { ...e, category, tags }
+        })
+    })
+    .flat()
 
 export default allEntities
