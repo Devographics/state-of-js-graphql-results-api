28f8f6fcb4198b400c191189c26fb7abf6472b35
SachaG <358832+SachaG@users.noreply.github.com>
2020-12-09 09:31:50 +0900

    Refactor with getStaticResolvers and getDynamicResolvers

--- src/resolvers/demographics.ts
+++ src/resolvers/demographics.ts
@@ -1,17 +1,11 @@
 import { Db } from 'mongodb'
-import { RequestContext, SurveyConfig } from '../types'
+import { RequestContext, SurveyConfig, ResolverStaticConfig } from '../types'
 import { Filters } from '../filters'
 import { useCache } from '../caching'
 import {
     computeParticipationByYear,
-    computeTermAggregationAllYearsWithCache,
-    computeTermAggregationSingleYearWithCache
 } from '../compute'
-
-interface DemographicsAggConfig {
-    survey: SurveyConfig
-    filters?: Filters
-}
+import { getStaticResolvers } from '../helpers'
 
 const computeParticipation = async (
     db: Db,
@@ -20,28 +14,15 @@ const computeParticipation = async (
     year?: number
 ) => useCache(computeParticipationByYear, db, [survey, filters, year])
 
-const getDemographicsResolvers = (id: string, options: any = {}) => ({
-    all_years: async (
-        { survey, filters }: DemographicsAggConfig,
-        args: any,
-        { db }: RequestContext
-    ) => computeTermAggregationAllYearsWithCache(db, survey, id, { ...options, filters }),
-    year: async (
-        { survey, filters }: DemographicsAggConfig,
-        { year }: { year: number },
-        { db }: RequestContext
-    ) => computeTermAggregationSingleYearWithCache(db, survey, id, { ...options, filters, year })
-})
-
 export default {
     Participation: {
         all_years: async (
-            { survey, filters }: DemographicsAggConfig,
+            { survey, filters }: ResolverStaticConfig,
             args: any,
             { db }: RequestContext
         ) => computeParticipation(db, survey, filters),
         year: async (
-            { survey, filters }: DemographicsAggConfig,
+            { survey, filters }: ResolverStaticConfig,
             { year }: { year: number },
             { db }: RequestContext
         ) => {
@@ -50,36 +31,36 @@ export default {
         }
     },
 
-    Country: getDemographicsResolvers('user_info.country_alpha3', {
+    Country: getStaticResolvers('user_info.country_alpha3', {
         sort: 'id',
         limit: 999,
         cutoff: 1
     }),
-    
-    LocaleStats: getDemographicsResolvers('user_info.locale', {
+
+    LocaleStats: getStaticResolvers('user_info.locale', {
         sort: 'id',
         limit: 100,
         cutoff: 1
     }),
-    
-    Source: getDemographicsResolvers('user_info.source.normalized'),
 
-    Gender: getDemographicsResolvers('user_info.gender', { cutoff: 1 }),
+    Source: getStaticResolvers('user_info.source.normalized'),
 
-    RaceEthnicity: getDemographicsResolvers('user_info.race_ethnicity.choices', { cutoff: 1 }),
-    
-    Salary: getDemographicsResolvers('user_info.yearly_salary', { limit: 100, cutoff: 1 }),
-    
-    CompanySize: getDemographicsResolvers('user_info.company_size', { limit: 100, cutoff: 1 }),
-    
-    WorkExperience: getDemographicsResolvers('user_info.years_of_experience', {
+    Gender: getStaticResolvers('user_info.gender', { cutoff: 1 }),
+
+    RaceEthnicity: getStaticResolvers('user_info.race_ethnicity.choices', { cutoff: 1 }),
+
+    Salary: getStaticResolvers('user_info.yearly_salary', { limit: 100, cutoff: 1 }),
+
+    CompanySize: getStaticResolvers('user_info.company_size', { limit: 100, cutoff: 1 }),
+
+    WorkExperience: getStaticResolvers('user_info.years_of_experience', {
         limit: 100,
         cutoff: 1
     }),
 
-    JobTitle: getDemographicsResolvers('user_info.job_title', {
+    JobTitle: getStaticResolvers('user_info.job_title', {
         cutoff: 1
     }),
 
-    KnowledgeScore: getDemographicsResolvers('user_info.knowledge_score', { limit: 100, cutoff: 1 })
+    KnowledgeScore: getStaticResolvers('user_info.knowledge_score', { limit: 100, cutoff: 1 })
 }
--- src/resolvers/other_tools.ts
+++ src/resolvers/other_tools.ts
@@ -1,37 +1,5 @@
-import {
-    computeTermAggregationAllYearsWithCache,
-    computeTermAggregationSingleYearWithCache
-} from '../compute'
-import { getOtherKey } from '../helpers'
-import { RequestContext, SurveyConfig } from '../types'
-import { Filters } from '../filters'
-
-interface OtherToolsConfig {
-    survey: SurveyConfig
-    id: string
-    filters?: Filters
-}
+import { getDynamicResolvers, getOtherKey } from '../helpers'
 
 export default {
-    OtherTools: {
-        all_years: async (
-            { survey, id, filters }: OtherToolsConfig,
-            args: any,
-            { db }: RequestContext
-        ) =>
-            computeTermAggregationAllYearsWithCache(db, survey, `tools_others.${getOtherKey(id)}`, {
-                filters
-            }),
-        year: async (
-            { survey, id, filters }: OtherToolsConfig,
-            { year }: { year: number },
-            { db }: RequestContext
-        ) =>
-            computeTermAggregationSingleYearWithCache(
-                db,
-                survey,
-                `tools_others.${getOtherKey(id)}`,
-                { filters, year }
-            )
-    }
+    OtherTools: getDynamicResolvers(id => `tools_others.${getOtherKey(id)}`)
 }
