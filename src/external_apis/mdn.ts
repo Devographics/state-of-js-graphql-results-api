f8ac2b36235e3770e5f01496928fe9a34155ca1c
SachaG <358832+SachaG@users.noreply.github.com>
2020-12-15 06:34:31 +0900

    Don't throw error when MDN fails

--- src/external_apis/mdn.ts
+++ src/external_apis/mdn.ts
@@ -30,6 +30,7 @@ export const fetchMdnResource = async (path: string) => {
         return normalizeMdnResource(json)
     } catch (error) {
         console.error(`an error occurred while fetching mdn resource`, error)
-        throw error
+        // throw error
+        return
     }
 }
